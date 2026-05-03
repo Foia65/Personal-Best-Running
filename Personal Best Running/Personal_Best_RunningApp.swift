2e0f8b76137190e27939ea82c6cbe2a54d13bda3
Francesco Foianesi <foiasoft@gmail.com>
2026-05-03 14:32:15 +0200

    Initial Commit

---
+++ Personal Best Running/Personal_Best_RunningApp.swift
@@ -0,0 +1,17 @@
+//
+//  Personal_Best_RunningApp.swift
+//  Personal Best Running
+//
+//  Created by Francesco Foianesi on 03/05/2026.
+//
+
+import SwiftUI
+
+@main
+struct Personal_Best_RunningApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
